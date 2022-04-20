a675e08b55b275fa06ed82c18fb96dd04d660915
Adotac <otacool89@gmail.com>
2022-04-20 15:33:17 +0800

    Activity for OnChanges

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('Montero, Joshua - pwTesting', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
 
@@ -31,4 +31,31 @@ describe('AppComponent', () => {
 
     expect(component.useLetters).toBeTrue();
   });
+
+  it('should click `Use Symbols` and useSymbols should be true', () => {
+    expect(component.useSymbols).toBeFalse();
+
+    let input = fixture.debugElement.query(
+      By.css('#withSymbols')
+    ).nativeElement;
+
+    input.click();
+    fixture.detectChanges();
+
+    expect(component.useSymbols).toBeTrue();
+  });
+
+  it('should click `Use Numbers` and useNumbers should be true', () => {
+    expect(component.useNumbers).toBeFalse();
+
+    let input = fixture.debugElement.query(
+      By.css('#withNumbers')
+    ).nativeElement;
+
+    input.click();
+    fixture.detectChanges();
+
+    expect(component.useNumbers).toBeTrue();
+  });
+
 });
